44d9d378b750f6aa986cd963396f14ca4ba30763
Tim Carlock <tcarlock@uber.com>
2018-05-07 00:14:22 -0700

    Updating export

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-export TreeMenu = require('./src/TreeMenu.jsx');
-export TreeNode = require('./src/TreeNode.jsx');
-export Utils = require('./src/TreeMenuUtils');
+module.exports = {
+  TreeMenu: require('./src/TreeMenu.jsx'),
+  TreeNode: require('./src/TreeNode.jsx'),
+  Utils: require('./src/TreeMenuUtils')
+};
