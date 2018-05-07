5af6cf46144d15bb357c18ec80bdd7ebd98bdb3c
Tim Carlock <tcarlock@uber.com>
2018-05-07 14:41:34 -0700

    Moving default props to external module to account for createElement not honoring them from the class

---
+++ src/nodeDefaultProps.js
@@ -0,0 +1,31 @@
+export default {
+  stateful: false,
+  collapsible: true,
+  collapsed: false,
+  checkbox : false,
+  onClick: (lineage) => {
+    console.log('Tree Node clicked: ' + lineage.join(' > '))
+  },
+  onCheckChange: (lineage) => {
+    console.log('Tree Node indicating a checkbox check state should change: ' + lineage.join(' > '))
+  },
+  onCollapseChange: (lineage) => {
+    console.log('Tree Node indicating collapse state should change: ' + lineage.join(' > '))
+  },
+  checked : false,
+  expandIconClass: '',
+  collapseIconClass: '',
+  labelFactory: (labelClassName, displayLabel) => {
+    return <label className={labelClassName}>{displayLabel}</label>
+  },
+  checkboxFactory: (className, isChecked) => {
+    return (
+      <input
+        className={className}
+        type='checkbox'
+        checked={isChecked}
+        onChange={noop}
+      />
+    )
+  }
+}
