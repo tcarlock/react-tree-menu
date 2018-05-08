a2d440102890970b114a7e731cb7fb8ff1191b5d
Tim Carlock <tcarlock@uber.com>
2018-05-07 23:39:24 -0700

    Updating checkbox logic

--- src/TreeMenu.jsx
+++ src/TreeMenu.jsx
@@ -109,15 +109,13 @@ export default class TreeMenu extends Component {
           children = dataToNodes(dataForNode.children, ancestor.concat(TreeNodeMixin.getNodeId(treeMenuProps, nodeProps, i)))
         }
 
-        const treeNodePros = TreeNodeMixin.getTreeNodeProps(treeMenuProps, nodeProps, ancestor, isRootNode, i)
-
         const newElement = React.createElement(
           TreeNode,
           Object.assign(
             {},
-            nodeProps,
-            treeNodePros,
             nodeDefaultProps
+            nodeProps,
+            TreeNodeMixin.getTreeNodeProps(treeMenuProps, nodeProps, ancestor, isRootNode, i),
           ),
           children
         )
